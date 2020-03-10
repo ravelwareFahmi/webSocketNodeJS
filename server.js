f63f4d46e120b49b765a133bb43e43e0380f958e
ravelwareFahmi <ravelware.fahmi@gmail.com>
2020-03-10 17:32:14 +0700

    update

---
+++ server.js
@@ -0,0 +1,9 @@
+const io = require('socket.io')(3000)
+io.on('connection', socket => {
+    // console.log('new User');
+    // socket.emit('chat-message', 'Hello World')
+    socket.on('send-chat-message', message => {
+        // console.log(message);
+        socket.broadcast.emit('chat-message', message)
+    })
+})
