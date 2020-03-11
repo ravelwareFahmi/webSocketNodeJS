446ff3732bd7737e4b88500ed7101f72ec8f6a06
ravelwareFahmi <ravelware.fahmi@gmail.com>
2020-03-11 10:28:42 +0700

    Fix Chat identified chat
    add change name with you

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ socket.on('user-disconnected', name => {
 messageForm.addEventListener('submit', e => {
     e.preventDefault()
     const message = messageInput.value
+    appendMessage(`You: ${message}`)
     socket.emit('send-chat-message', message)
     messageInput.value = ''
 })
